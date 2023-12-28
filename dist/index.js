5399a159dd0cf7c2a7bd2b2035c4c0b43dffe57e
David Hiendl <david.hiendl@dhswt.de>
2023-12-28 20:35:01 +0100

    fix missing .docker/config.json file path argument

--- dist/index.js
+++ dist/index.js
@@ -62486,7 +62486,7 @@ try {
   }
 
   const registryAuthJson = {auths: {}};
-  mergeExistingDockerAuthJson(registryAuthJson);
+  mergeExistingDockerAuthJson(registryAuthJson, dockerConfigFile);
   addCiRegistryAuth(ci_registry, registryAuthJson);
   mergeArgRegistryAuthJson(registryAuthJson);
   writeRegistryAuthJson(registryAuthJson, dockerConfigFile);
